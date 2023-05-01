e58e465a0ca0e1135981fdd7ae36b42616343e27
Niimaii <nimaicgomes@gmail.com>
2023-04-30 23:12:35 -0300

    React Query fetching & mutation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,54 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+
+const POSTS = [
+  { id: 1, title: 'Post 1' },
+  { id: 2, title: 'Post 2' },
+];
+
 function App() {
-  return <h1>React Query</h1>;
+  // Initiate useQueryClient (This is in reference to queryClient in main.jsx)
+  const queryClient = useQueryClient();
+  const postQuery = useQuery({
+    // Unique query identifier
+    queryKey: ['posts'],
+    // Function
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: (title) => {
+      return wait(1000).then(() =>
+        POSTS.push({ id: crypto.randomUUID(), title })
+      );
+    },
+    // When a mutation is successful, replace the old request data
+    onSuccess: () => {
+      queryClient.invalidateQueries(['posts']);
+    },
+  });
+
+  if (postQuery.isLoading) return <h1>Loading...</h1>;
+  if (postQuery.isError) {
+    return <pre>{JSON.stringify(postQuery.error)}</pre>;
+  }
+
+  return (
+    <div>
+      {postQuery.data.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+      <button
+        disabled={newPostMutation.isLoading}
+        onClick={() => newPostMutation.mutate('New Post')}
+      >
+        Add New
+      </button>
+    </div>
+  );
+}
+
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 export default App;
